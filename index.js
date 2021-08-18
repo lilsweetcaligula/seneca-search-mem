bc8701bc0952381759e9532935151f9d9e54e2cf
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-18 07:34:17 +0300

    Initialize the project

---
+++ index.js
@@ -0,0 +1,34 @@
+const Seneca = require('seneca')
+const SenecaSearchMem = require('./search-mem')
+
+
+async function run() {
+  const seneca = Seneca()
+
+
+  seneca.use(SenecaSearchMem, {
+    search: {
+      fields: ['name', 'extra'], // <~~ query-able fields
+
+      searchOptions: {
+        fuzzy: 0.2
+      }
+    }
+  })
+
+
+  seneca.use('promisify')
+
+
+  const out = await seneca.post('sys:search,cmd:search', {
+    query: 'bob'
+  })
+
+  console.dir(out, { depth: 32 }) // dbg
+
+
+  return
+}
+
+
+run()
---
+++ search-mem.js
@@ -0,0 +1,64 @@
+const Minisearch = require('minisearch')
+
+
+function search_mem(options) {
+  const seneca = this
+
+
+  if (null == options.search) {
+    return seneca.fail('The "search" option is required')
+  }
+
+  const { search: search_config } = options
+  const minisearch = new Minisearch(search_config)
+
+
+  /*
+   * BEGIN: DEBUG
+   */
+
+  minisearch.addAll([
+    { id: '1000', name: 'bob' },
+    { id: '1001', name: 'boba' },
+    { id: '1002', name: 'foo', extra: 'bobb' }
+  ])
+
+  /*
+   * END: DEBUG
+   */
+
+
+  seneca.add('sys:search,cmd:search', function (msg, reply) {
+    if (null == msg.query) {
+      return {
+        ok: false,
+        why: 'invalid-field',
+        details: {
+          path: ['query'],
+          why_exactly: 'required'
+        }
+      }
+    }
+
+    const { query } = msg
+
+
+    /* NOTE: For more information, please see documentation at:
+     *
+     * https://www.npmjs.com/package/minisearch
+     *
+     */
+    const out = minisearch.search(query)
+
+    console.dir(out, { depth: 32 }) // dbg
+
+
+    return reply(null, { ok: true })
+  })
+
+
+  return
+}
+
+
+module.exports = search_mem
